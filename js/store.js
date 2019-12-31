2ac8ed9a39721cf7845268116c279a844b3a3cc3
huydrk <57650441+huydrk@users.noreply.github.com>
2020-01-01 01:14:21 +0700

    thay đổi trang store
    link toàn bộ quần áo vào store chính

--- js/store.js
+++ js/store.js
@@ -1,78 +1,79 @@
- //khai báo biến slideIndex đại diện cho slide hiện tại
- var slideIndex;
- // KHai bào hàm hiển thị slide
- function showSlides() {
-     var i;
-     var slides = document.getElementsByClassName("ep_mySlides");
-     var dots = document.getElementsByClassName("ep_dot");
-     for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-     }
-     for (i = 0; i < dots.length; i++) {
-         dots[i].className = dots[i].className.replace(" active", "");
-     }
+//khai báo biến slideIndex đại diện cho slide hiện tại
+var slideIndex;
+// KHai bào hàm hiển thị slide
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("ep_mySlides");
+    var dots = document.getElementsByClassName("ep_dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
 
-     slides[slideIndex].style.display = "block";
-     dots[slideIndex].className += " active";
-     //chuyển đến slide tiếp theo
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+    //chuyển đến slide tiếp theo
 
-     //nếu đang ở slide cuối cùng thì chuyển về slide đầu
-     if (slideIndex > slides.length - 1) {
-         slideIndex = 0
-     }
-     //tự động chuyển đổi slide sau 5s
-     setTimeout(showSlides, 5000);
- }
- //chạy hình ảnh khi bấm nut
- var slideIndex = 1;
- showSlides(slideIndex);
+    //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+    if (slideIndex > slides.length - 1) {
+        slideIndex = 0
+    }
+    //tự động chuyển đổi slide sau 5s
+    setTimeout(showSlides, 5000);
+}
+//chạy hình ảnh khi bấm nut
+var slideIndex = 1;
+showSlides(slideIndex);
 
- function plusSlides(n) {
-     showSlides(slideIndex += n);
- }
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
- function currentSlide(n) {
-     showSlides(slideIndex = n);
- }
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
- function showSlides(n) {
-     var i;
-     var slides = document.getElementsByClassName("ep_mySlides");
-     var dots = document.getElementsByClassName("ep_dot");
-     if (n > slides.length) { slideIndex = 1 }
-     if (n < 1) { slideIndex = slides.length }
-     for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-     }
-     for (i = 0; i < dots.length; i++) {
-         dots[i].className = dots[i].className.replace("ep_active", "");
-     }
-     slides[slideIndex - 1].style.display = "block";
-     dots[slideIndex - 0].className += " ep_active";
- }//chạy hình ảnh khi bấm nut
- var slideIndex = 1;
- showSlides(slideIndex);
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("ep_mySlides");
+    var dots = document.getElementsByClassName("ep_dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("ep_active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 0].className += " ep_active";
+}//chạy hình ảnh khi bấm nut
+var slideIndex = 1;
+showSlides(slideIndex);
 
- function plusSlides(n) {
-     showSlides(slideIndex += n);
- }
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
- function currentSlide(n) {
-     showSlides(slideIndex = n);
- }
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
- function showSlides(n) {
-     var i;
-     var slides = document.getElementsByClassName("ep_mySlides");
-     var dots = document.getElementsByClassName("ep_dot");
-     if (n > slides.length) { slideIndex = 1 }
-     if (n < 1) { slideIndex = slides.length }
-     for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-     }
-     for (i = 0; i < dots.length; i++) {
-         dots[i].className = dots[i].className.replace("ep_active", "");
-     }
-     slides[slideIndex - 1].style.display = "block";
-     dots[slideIndex - 1].className += " ep_active";
- }
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("ep_mySlides");
+    var dots = document.getElementsByClassName("ep_dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("ep_active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " ep_active";
+}
+
